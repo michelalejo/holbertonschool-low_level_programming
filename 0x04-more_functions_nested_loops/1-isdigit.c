c51b5f780b5e06b6002df4401eea41560d8556e8
michelalejo <michelmolina65@gmail.com>
2019-09-26 17:43:55 +0000

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * _isupper - function that checks for uppercase character
- * @c: takes in a character
- * Return: 1 for uppercase and 0 for lowercase
+ * _isdigit - function that check if it's a digit
+ * @c: takes in a character/digit
+ * Return: 1 if digit and 0 if anything else
  */
 int _isupper(int c)
 {
