5571b4df583982fe9b35de760da569454507aaf2
michelalejo <michelmolina65@gmail.com>
2019-09-26 19:54:56 +0000

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_line - function that prints a straight line
+ * @n: takes in an integer
+ */
+void print_line(int n)
+{
+	char r = '_';
+	int f = n;
+
+	if (n > 0)
+	{
+		for (; f > 0; f--)
+			_putchar(r);
+	}
+	_putchar('\n');
+}
