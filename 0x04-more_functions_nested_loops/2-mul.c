2766a889079e7e73222d43a650aa0c69f6012d8e
michelalejo <michelmolina65@gmail.com>
2019-09-26 17:56:36 +0000

    2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
- * _isdigit - function that check if it's a digit
- * @c: takes in a character/digit
- * Return: 1 if digit and 0 if anything else
+ * mul - function that multiple two integers
+ * @a: takes in an integer
+ * @b: takes in an integer
+ * Return: returns result of a * b
  */
 int mul(int a, int b)
 {
