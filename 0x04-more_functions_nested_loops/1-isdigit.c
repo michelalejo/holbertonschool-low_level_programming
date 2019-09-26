5eee72ad549fea16db62a42a2eaeacd9b6f65660
michelalejo <michelmolina65@gmail.com>
2019-09-26 17:43:29 +0000

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,11 +4,9 @@
  * @c: takes in a character
  * Return: 1 for uppercase and 0 for lowercase
  */
-int _isdigit(int c)
+int _isupper(int c)
 {
-	if ((c >= 'a' && c <= 'z') && ((c >= 'A' && c <= 'Z'))
-		{
+		if (c >= '0' && c <= '9')
 			return (1);
-		}
 		return (0);
 }
