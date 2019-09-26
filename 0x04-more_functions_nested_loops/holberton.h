7a66d47363be0c3a738bfe5c6109555ca2e8ceb9
michelalejo <michelmolina65@gmail.com>
2019-09-26 18:34:42 +0000

    Fix

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -6,3 +8,4 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+#endif
