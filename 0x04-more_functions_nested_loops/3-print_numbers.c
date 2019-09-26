a9ce12f8adf81adaec8c55c3d863e0f1d603cf90
michelalejo <michelmolina65@gmail.com>
2019-09-26 18:19:41 +0000

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,10 +6,10 @@
 void print_most_numbers(void)
 {
 	char c;
-		for (c = 0; c < 10; c++)
-		{
-			if (c != 2 && c != 4)
-				_putchar(c);
-		}
+	for (c = 0; c < 10; c++)
+	{
+		if (c != 2 && c != 4)
+			_putchar(c);
+	}
 	_putchar('\n');
 }
