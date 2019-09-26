e0a25f7d37488231c46bb308b91e97ec1476e482
michelalejo <michelmolina65@gmail.com>
2019-09-26 17:53:40 +0000

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _isdigit - function that check if it's a digit
+ * @c: takes in a character/digit
+ * Return: 1 if digit and 0 if anything else
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
