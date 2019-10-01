a2e4cc80acb11208a8a30e0fd7630ebebf045f3e
michelalejo <michelmolina65@gmail.com>
2019-10-01 19:44:52 +0000

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * print_rev - text to be print in reverse.
+ * rev_string - text to be print in reverse.
  * @s: s have the string to print.
  * void: void
  */
-void print_rev(char *s)
+void rev_string(char *s)
 {
 	int a, b, temp;
 
