b3389f1b728aa3125434ed19c1bbe71a14a1c195
michelalejo <michelmolina65@gmail.com>
2019-10-03 22:14:28 +0000

    6-cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: takes in a string
+ * Return: s, the string
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+	{
+		s[0] -= 32;
+
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			switch (s[i])
+			{
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			case ' ':
+			case '\n':
+			case '\t':
+
+				if (s[i + 1] > 96 && s[i + 1] < 123)
+					s[i + 1] -= 32;
+			}
+		}
+		return (s);
+	}
