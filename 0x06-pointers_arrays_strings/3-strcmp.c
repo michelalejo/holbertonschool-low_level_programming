633644871362af8734cb714a2aefbb7166eeeaff
michelalejo <michelmolina65@gmail.com>
2019-10-03 20:28:31 +0000

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 /**
- * _strncpy - simulate the way strncpy works.
- * @src: q = to the s1
- * @dest: p = to the s2
- * @n: the number of leters it have to print
- * Return: dest.
+ * _strcmp - simulate the way strcmp works.
+ * @s1: q = to the s1.
+ * @s2: p = to the s2.
+ * Return: s1 - s2 or 0.
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-     
+
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-		if (s1 != s2)
+	{
+		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
+	}
 	return (0);
-        
 }
