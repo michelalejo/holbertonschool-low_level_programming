4061edbc01392e7c519d9ea36f7895a46793326b
michelalejo <michelmolina65@gmail.com>
2019-10-04 00:27:31 +0000

    100-print_number

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,45 +1,26 @@
 #include "holberton.h"
 
 /**
- * print_number - prints the number
- * @n: number to print
- * Return:none
+ * print_number - Prints an integer.
+ * @n: the integer.
  */
 
 void print_number(int n)
 {
-	int i, digit, place, fake_bool;
-	long j;
-	char num[1000];
-
-	place = 0;
-	fake_bool = 0;
-	j = n;
-	if (j < 0)
+	if (n > 9 || n < -9)
 	{
-		j = j * -1;
-		fake_bool = 1;
+		print_number(n / 10);
+		if (n < 0)
+			n = (n % 10) * -1;
+		_putchar ((n % 10) + '0');
 	}
-	while ((j / 10) != 0)
+	else
 	{
-		digit = j % 10;
-		num[place] = digit;
-		place++;
-		j = j / 10;
-	}
-	num[place] = j;
-	if (fake_bool == 1)
-	{
-		place++;
-		num[place] = '-';
-	}
-	for (i = place; i >= 0; i--)
-	{
-		if (num[i] != '-')
+		if (n < 0)
 		{
-			_putchar(num[i] + '0');
-		}
-		else
 			_putchar('-');
+			n *= -1;
+		}
+		_putchar(n + '0');
 	}
 }
