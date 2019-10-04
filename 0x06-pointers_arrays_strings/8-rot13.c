ede1a0e7e23f93f14543c092cf4e01501be343ea
michelalejo <michelmolina65@gmail.com>
2019-10-04 00:15:56 +0000

    8-rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,29 +1,43 @@
 #include "holberton.h"
 
 /**
- * rot13 - conver string o rot13
- * @s: string
- * Return: string
+ * rot13 - Desciphrate
+ * @s: The array
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 char *rot13(char *s)
 {
-	int i;
+	int i, j;
+	char let[52] = {'a',
+			'b', 'c', 'd', 'e', 'f', 'g', 'h',
+			'i', 'j', 'k', 'l', 'm', 'n', 'o',
+			'p', 'q', 'r', 's', 't', 'u', 'v',
+			'w', 'x', 'y', 'z', 'A', 'B', 'C',
+			'D', 'E', 'F', 'G', 'H', 'I', 'J',
+			'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+			'R', 'S', 'T', 'U', 'V', 'W', 'X',
+			'Y', 'Z'};
+	char rot[52] = {'n', 'o', 'p', 'q', 'r',
+			's', 't', 'u', 'v', 'w', 'x', 'y',
+			'z', 'a', 'b', 'c', 'd', 'e', 'f',
+			'g', 'h', 'i', 'j', 'k', 'l', 'm',
+			'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+			'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B',
+			'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+			'K', 'L', 'M'};
 
-	i = 0;
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+		for (j = 0; j <= 51; j++)
 		{
-			s[i] = (s[i] + 13);
-		}
-		else
-			while ((s[i] >= 'n' && s[i] <= 'z') ||
-			       (s[i] >= 'N' && s[i] <= 'Z'))
+			if (s[i] == let[j])
 			{
-				s[i] = (s[i] - 13);
+				s[i] = rot[j];
+				break;
 			}
-		i++;
+		}
 	}
 	return (s);
 }
