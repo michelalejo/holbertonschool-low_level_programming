8b4b8c1f55dd2a08eff20c99d4fa907ed9f7bb0c
michelalejo <michelmolina65@gmail.com>
2019-10-04 04:04:19 +0000

    holberton.h

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -14,6 +14,8 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+int _strlen(char *s);
 void print_buffer(char *b, int size);
+void print_line(char *b, int position, int n);
 
 #endif /* HOLBERTON_H */
