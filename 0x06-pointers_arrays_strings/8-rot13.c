6925dc4a77632d62899a4b0f197f3f2f902f02a4
michelalejo <michelmolina65@gmail.com>
2019-10-04 00:06:25 +0000

    8-rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * rot13 - this functions encrypt using the rot13 algorithm
+(* a blank line
+*@a: this parameter is the string to encrypt
+* Description: this function encrypts using the rot13 algorithm)?
+(* section header: the header of this function is holberton.h)*
+* Return: this return a char.
+*/
+har *rot13(char *a)
+{
+	int j;
+	char charsupp[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char charslow[] = "nopqrstuvwxyzabcdefghijklm";
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		if ((a[j] >= 'A' && a[j] < 'Z') || (a[j] >= 'a' && a[j] < 'z'))
+		{
+			a[j] = (a[j] - 65 > 25) ?
+				charslow[s[i] - 97] : charsupp[s[i] - 65];
+		}
+	}
+	return (s);
+}
