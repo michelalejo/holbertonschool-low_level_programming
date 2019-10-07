8621726d87777d1ceac59e4a125e1f0a92191ab0
michelalejo <michelmolina65@gmail.com>
2019-10-07 20:50:54 +0000

    fix

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _memset - Write a function that fills memory with a constant byte.
+ * @s: pointer
+ * @b: char
+ * @n: int
+ * Return: 0
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i, f = n;
+	char *a = s;
+
+	for (i = 0; i < f; i++)
+		a[i] = b;
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _memset - Write a function that fills memory with a constant byte.
+ * @s: pointer
+ * @b: char
+ * @n: int
+ * Return: 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
