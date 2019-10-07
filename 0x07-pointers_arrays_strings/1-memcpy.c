58c8d0316d337be4512716b28cfb265ccb0b3b2a
michelalejo <michelmolina65@gmail.com>
2019-10-07 21:21:14 +0000

    1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 /**
- * _memset - Write a function that fills memory with a constant byte.
- * @s: pointer
- * @b: char
- * @n: int
- * Return: 0
+ * _memcpy - Write a function that copies memory area.
+ * @dest: pointer
+ * @src: pointer
+ * @n: n
+ * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int i, f = n;
-	char *t = (char*)dest;
-	char *s = (char*)src;
+	char *t = (char *)dest;
+	char *s = (char *)src;
 
-       	for( i = 0; i < f; i++)
+	for (i = 0; i < f; i++)
 		t[i] = s[i];
 	return (dest);
 }
