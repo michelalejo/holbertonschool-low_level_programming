4e8a49c396e87ad071001f6edf011f622ceb8b4a
michelalejo <michelmolina65@gmail.com>
2019-10-07 23:41:24 +0000

    4-strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strpbrk - Write a function that gets the length of a prefix substring.
+ * @s: Pointer
+ * @accept: Pointer
+ * Return: 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+	return (NULL);
+}
