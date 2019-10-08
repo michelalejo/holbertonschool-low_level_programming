9e15c150850b60a9e32eb066dbd47292c7becb7a
michelalejo <michelmolina65@gmail.com>
2019-10-08 00:21:13 +0000

    7-print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,15 +1,14 @@
 #include "holberton.h"
 /**
- * _strpbrk - Write a function that gets the length of a prefix substring.
- * @s: Pointer
- * @accept: Pointer
- * Return: 0
+ * print_chessboard -  prints the chessboard.
+ * @a: char a
+ * Return: Always 0.
  */
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; a[i] < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
