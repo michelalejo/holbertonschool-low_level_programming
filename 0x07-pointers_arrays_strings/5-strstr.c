b9ebe44b07a59aecaee0fc8262f7c59f3ab27366
michelalejo <michelmolina65@gmail.com>
2019-10-08 00:51:52 +0000

    5-strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strstr - Function that locates a substring..
+ * @haystack: String to be compared
+ * @accept: Substring to be located
+ * Return: Null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (k = 1, j = 0; needle[j] != '\0'; j++, k++)
+		{
+			if (haystack[k] != needle[j] || haystack[k] == '\0')
+				break;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i);
+	}
+	return (NULL);
+}
