cc50f35977ef8c6c2915e761002337c7331023be
michelalejo <michelmolina65@gmail.com>
2019-10-10 20:51:02 +0000

    5-sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,29 +1,31 @@
 #include "holberton.h"
 /**
- * _sqrt_recursion - calculate the sqare root of a number
- * @n: integer
- * Return: result of the sqare root of n
+ * _sqrt_recursion - Function returns the natural square root of a number.
+ * @n: Integer.
+ * Return: Result of the square.
  */
 int _sqrt_recursion(int n)
 {
 	int result = 0;
+
 	result = _find(n, 0);
 	return (result);
 }
 
 /**
- * _find - calculate the sqare root of a number
+ * _find - Find the square.
  * @n: integer
- * @factor:integer
+ * @xD:integer
  * Return: result of the sqare root of n
  */
-int _find(int n, int factor)
+int _find(int n, int xD)
 {
 	int result = 0;
-	if (factor * factor > n)
+
+	if (xD * xD > n)
 		return (-1);
-	if (factor * factor == n)
-		return (factor);
-	result = _find(n, factor + 1);
+	if (xD * xD == n)
+		return (xD);
+	result = _find(n, xD + 1);
 	return (result);
 }
