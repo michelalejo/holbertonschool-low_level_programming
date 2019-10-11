34f614171b816c5ce9d048f43574d3cfcf45308d
michelalejo <michelmolina65@gmail.com>
2019-10-11 23:56:06 +0000

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,30 +1,32 @@
 #include "holberton.h"
-#include <stdio.h>
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+ * _atoi - this function converts a string to integer
+* a blank line
+*@s: the string for convert to integer
+* Description: this funtion converts a string to integer)?
+* section header: the header of this function is holberton.h)*
+* Return: this funtion returns the result
+*/
+int _atoi(char *s)
 {
-	int n;
+	int c, sign;
+	unsigned int n;
 
-	n = _atoi("98");
-	printf("%d\n", n);
-	n = _atoi("-402");
-	printf("%d\n", n);
-	n = _atoi("          ------++++++-----+++++--98");
-	printf("%d\n", n);
-	n = _atoi("214748364");
-	printf("%d\n", n);
-	n = _atoi("0");
-	printf("%d\n", n);
-	n = _atoi("Suite 402");
-	printf("%d\n", n);
-	n = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-	printf("%d\n", n);
-	n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-	printf("%d\n", n);
-	return (0);
+	sign = 1;
+	c = 0;
+	n = 0;
+
+
+	while ((s[c] < '0' || s[c] > '9') && (s[c] != '\0'))
+	{
+		if (s[c] == '-')
+			sign *= -1;
+		c++;
+	}
+	while ((s[c] >= '0') && (s[c] <= '9'))
+	{
+		n = n * 10 + (s[c] - '0');
+		c++;
+	}
+	return (n * sign);
 }
