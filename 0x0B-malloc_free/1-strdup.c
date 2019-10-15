6e7fc2e0e41c1b91390116ce55b65010a149d4f3
michelalejo <michelmolina65@gmail.com>
2019-10-15 19:49:48 +0000

    1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - Function that creates an array of chars.
+ * @size:  Size of the array.
+ * @c: Value of the array.
+ * Return: Return NULL if is 0/fail.
+ **/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int j;
+	char *i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	i = malloc(sizeof(char) * size);
+	if (i == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < size; j++)
+	{
+		*(i + j) = c;
+	}
+	return (i);
+}
