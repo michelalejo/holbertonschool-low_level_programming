ef821c4813d18811f720a333148fa2f64fec9dea
michelalejo <michelmolina65@gmail.com>
2019-10-15 19:25:20 +0000

    0-create_array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ **/
+char *create_array(unsigned int size, char c)
+{
+	char *i;
+
+	i = malloc(sizeof(char) * c)
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	if 
+	
+}
