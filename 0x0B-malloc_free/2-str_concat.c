3be84d6b26af8b89f5dcee5ceb9193617db8ab80
michelalejo <michelmolina65@gmail.com>
2019-10-15 23:16:57 +0000

    2-str_concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - Function that returns a pointer to a newly allocated in memory.
+ * @str:  Size of the array.
+ * Return: Return NULL if str and s is null
+ **/
+char *str_concat(char *s1, char *s2)
+{
+	int j, i, k, a;
+	char *s;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
+	{
+		return ('\0');
+	}
+	for (k = 0; k < i; k++)
+	{
+		s[k] = s1[k];
+	}
+	for (a = 0; a < j; a++)
+	{
+		s[i + a] = s2[a];
+	}
+	return (s);
+}
