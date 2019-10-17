0c0e905e60bf5c85ca93c420477fa11ca432317a
michelalejo <michelmolina65@gmail.com>
2019-10-17 20:39:28 +0000

    0-malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *a;
+
+	a = malloc(b);
+        if (a == NULL)
+		exit(98);
+	return (a);
+}
