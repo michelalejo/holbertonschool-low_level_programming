22ac854f320cd69e997465aac956cf46e902f41e
michelalejo <michelmolina65@gmail.com>
2019-10-22 03:09:03 +0000

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,13 +6,41 @@
  *@name: The name of the dog.
  *@age: The age of the dog.
  *@owner: The owner of the dog.
+ *return: 0
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d != NULL)
+	dog_t *d;
+	int i, j, k, a;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
 	}
+	for (j = 0; owner[j] != '\0'; j++)
+	{
+	}
+	d->name = malloc(sizeof(char) * i + 1);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	for (k = 0; k < (i + 1); k++)
+		d->name[k] = name[k];
+	d->age = age;
+	d->owner = malloc(sizeof(char) * (j + 1));
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	for (a = 0; a < (j + 1); a++)
+		d->owner[a] = owner[a];
+	return (d);
 }
