347cad32ca485a66d2d678920e1afcbd84495657
michelalejo <michelmolina65@gmail.com>
2019-10-22 03:11:30 +0000

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	int i, j, k, a;
+	int i, j, k, l;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
@@ -20,11 +20,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 	for (i = 0; name[i] != '\0'; i++)
-	{
-	}
+		;
 	for (j = 0; owner[j] != '\0'; j++)
-	{
-	}
+		;
 	d->name = malloc(sizeof(char) * i + 1);
 	if (d->name == NULL)
 	{
@@ -34,14 +32,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (k = 0; k < (i + 1); k++)
 		d->name[k] = name[k];
 	d->age = age;
-	d->owner = malloc(sizeof(char) * (j + 1));
+	d->owner = malloc(sizeof(char) * j + 1);
 	if (d->owner == NULL)
 	{
 		free(d->name);
 		free(d);
 		return (NULL);
 	}
-	for (a = 0; a < (j + 1); a++)
-		d->owner[a] = owner[a];
+	for (l = 0; l < (j + 1); l++)
+		d->owner[l] = owner[l];
 	return (d);
 }
