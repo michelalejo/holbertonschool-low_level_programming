11cb754cbe2ff019a6d671bcc8d4c6d4409d84d0
michelalejo <michelmolina65@gmai.com>
2019-10-23 21:16:52 +0000

    2-int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ *int_index - Function that given as parameter of each element in array.
+ *@array: Array.
+ *@size: Name to be print.
+ *@cpm: Funtion.
+ *Return: If size > -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, a;
+
+	a = size;
+	if (array && size && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < a; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
