608399c56f90c795cf3a925942066b76deee537b
michelalejo <michelmolina65@gmail.com>
2019-11-01 03:30:57 +0000

    3-add_node_end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,18 +8,32 @@
  * @str: Pointer to str.
  * Return: The elements it prints.
  */
-list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str)
 {
-	int i;
-	list_t *a = malloc(sizeof(list_t);
+	int i = 0;
+	list_t *a, *b;
 
-        if (a == NULL);
-	        return (NULL);
-	while(a->len != NULL)
+	a = malloc(sizeof(list_t));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	while (*(str + i) != '\0')
 		i++;
 	a->str = strdup(str);
 	a->len = i;
-        a->next = *head;
-	*head = a;
+	a->next = NULL;
+
+	if (*head != NULL)
+	{
+		b = *head;
+		while (b->next != NULL)
+			b = b->next;
+		b->next = a;
+	}
+	else
+	{
+		*head = a;
+	}
 	return (a);
 }
