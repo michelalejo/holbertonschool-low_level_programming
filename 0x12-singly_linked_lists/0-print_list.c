5a12d2c6b1e3651844d5f98c5add6bdbe4e9739d
Holberton <Holberton@CAL-0020.hbtn.io>
2019-10-31 19:25:47 -0500

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,29 @@
-#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include "lists.h"
 /**
 *
 *
 *
 */
+size_t print_list(const list_t *h)
+{
+        size_t i = 0;
+	list_t *a;
+
+	a = h;
+	while (a != NULL)
+	{
+		if (a->str == NULL)
+		{
+
+			printf("[0] (nil)\n");
+		}
+		else
+		printf("[%d] %s\n", a->len, a->str);
+		a = a->next;
+		i++;
+	}
+	return (i);
+}
