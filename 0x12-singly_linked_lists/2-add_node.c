b4c19c1b5800ab2064b94ce4e7cf71e2813286ab
michelalejo <michelmolina65@gmail.com>
2019-11-01 03:19:56 +0000

    2-add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,13 +10,17 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	int i;
-	list_t *a = malloc(sizeof(list_t);
+	int i = 0;
+	list_t *a = malloc(sizeof(list_t));
 
-        if (a == NULL);
-	        return (NULL);
-	while(a->len != NULL)
+        if (a == NULL)
+	{
+		return (NULL);
+	}
+	while(str[i] != '\0')
+	{
 		i++;
+	}
 	a->str = strdup(str);
 	a->len = i;
         a->next = *head;
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_node - Function that adds nodes at the beginning of list_t list
+ * @head: Pointer to head.
+ * @str: Pointer to str.
+ * Return: The elements it prints.
+ */
+list_t *add_node_end(list_t **head, const char *str);
+{
+	int i;
+	list_t *a = malloc(sizeof(list_t);
+
+        if (a == NULL);
+	        return (NULL);
+	while(a->len != NULL)
+		i++;
+	a->str = strdup(str);
+	a->len = i;
+        a->next = *head;
+	*head = a;
+	return (a);
+}
