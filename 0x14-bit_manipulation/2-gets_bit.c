6bfa01bf2aa29ff3cd746765742df39fa4b1c015
michelalejo <michelmolina65@gmail.com>
2019-11-08 00:12:49 +0000

    2-gets_bit

---
+++ 0x14-bit_manipulation/2-gets_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * get_bit - Funtion that return the value of a given index..
+ *@n: N.
+ *@index: Index.
+ * Return: Index or -1.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+	*n = *n >> index & 1;
+	return (0);
+}
