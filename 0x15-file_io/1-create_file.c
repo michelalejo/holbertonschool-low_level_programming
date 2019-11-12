21b1b4b3118a8297d4c4dc6e1a7c77cb27889865
michelalejo <michelmolina65@gmail.com>
2019-11-12 22:58:22 +0000

    1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	f = open(filename, O_CREAT | O_WRONLY, 0600);
+	f = open(filename, O_CREAT | O_TURNC | O_WRONLY, 0600);
 
 	if (f == -1)
 	{
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 	if (s == -1)
 	{
 		close(f);
-		return (0);
+		return (1);
 	}
 
 	close(f);
