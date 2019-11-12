c64993e928b5dcd69f01565008b407a3bdc22592
michelalejo <michelmolina65@gmail.com>
2019-11-12 23:14:44 +0000

    2-append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,7 +9,6 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int f, d, i;
 
-
 	if (filename == NULL)
 		return (-1);
 
@@ -30,7 +29,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (d == -1)
 	{
 		close(f);
-		return (1);
+		return (-1);
 	}
 
 	close(f);
