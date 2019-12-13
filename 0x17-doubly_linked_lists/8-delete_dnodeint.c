13b9ff9857575f90886ec4de0e535d7bd11682c8
michelalejo <michelmolina65@gmail.com>
2019-12-13 21:11:42 +0000

    files add

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_dnodeint - Funtion that adds a new node at the beginning of a list.
+ * @head: Head of the list.
+ * @n: Item to be created and added to the list.
+ * Return: The new node.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	*head = new;
+	return (new);
+}
