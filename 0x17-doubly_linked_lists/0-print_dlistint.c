c44d33ba933080d831367ce9746d5b8e34021443
michelalejo <michelmolina65@gmail.com>
2019-12-13 21:25:37 +0000

    fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,14 @@
 #include "lists.h"
 /**
- * add_dnodeint - Funtion that adds a new node at the beginning of a list.
- * @head: Head of the list.
- * @n: Item to be created and added to the list.
- * Return: The new node.
+ * print_dlistint - Function that prints all the elements of a list.
+ * @h: Head of the list.
+ * Return: The times it prints.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
 
-	for(count = 0; h != NULL; count++)
+	for (count = 0; h != NULL; count++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
