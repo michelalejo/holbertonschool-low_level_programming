11202250a43d2cfadbc9bb9a0e3f7941f43f9fb5
michelalejo <michelmolina65@gmail.com>
2020-01-18 20:08:12 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,28 +4,29 @@
  * @size: Size of the array.
  * Return: Returns a pointer to the newly created hash table or NULL.
  */
- hash_table_t *hash_table_create(unsigned long int size)
+hash_table_t *hash_table_create(unsigned long int size)
 {
-    unsigned long int i = 0;
-    hash_table_t *n_hash = malloc(sizeof(hash_table_t) * 1);
-    if (n_hash == NULL)
-    {
-        free(n_hash);
-        return (NULL);
-    }
+	unsigned long int i = 0;
+	hash_table_t *n_hash = malloc(sizeof(hash_table_t) * 1);
 
-    n_hash->array = malloc(sizeof(hash_node_t*) * size);
-    if (n_hash->array == NULL)
-    {
-        free(n_hash);
-        return (NULL);
-    }
+	if (n_hash == NULL)
+	{
+		free(n_hash);
+		return (NULL);
+	}
 
-    for (i = 0; i < size ; i++)
-    {
-        n_hash->array[i] = NULL;
-    }
-    n_hash->size = size;
+	n_hash->array = malloc(sizeof(hash_node_t *) * size);
+	if (n_hash->array == NULL)
+	{
+		free(n_hash);
+		return (NULL);
+	}
 
-    return (n_hash);
+	for (i = 0; i < size ; i++)
+	{
+		n_hash->array[i] = NULL;
+	}
+	n_hash->size = size;
+
+	return (n_hash);
 }
