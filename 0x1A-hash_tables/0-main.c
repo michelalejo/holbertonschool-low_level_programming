536d50b909d75e743b1ca0480f32f49a86c54153
michelalejo <michelmolina65@gmail.com>
2020-01-18 12:57:01 -0500

    Update 0-main.c

--- 0x1A-hash_tables/0-main.c
+++ 0x1A-hash_tables/0-main.c
@@ -1,6 +1,3 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
 
 /**
@@ -15,30 +12,4 @@ int main(void)
     ht = hash_table_create(1024);
     printf("%p\n", (void *)ht);
     return (EXIT_SUCCESS);
-}
-
-hash_table_t *hash_table_create(unsigned long int size)
-{
-    unsigned long int i = 0;
-    hash_table_t *n_hash = malloc(sizeof(hash_table_t) * 1);
-    if (n_hash == NULL)
-    {
-        free(n_hash);
-        return (NULL);
-    }
-
-    n_hash->array = malloc(sizeof(hash_node_t*) * size);
-    if (n_hash->array == NULL)
-    {
-        free(n_hash);
-        return (NULL);
-    }
-
-    for (i = 0; i < size ; i++)
-    {
-        n_hash->array[i] = NULL;
-    }
-    n_hash->size = size;
-
-    return (n_hash);
 }
