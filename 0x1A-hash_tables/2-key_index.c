c589bdadbe6c28ce17028ac031f87a5228eecdaa
michelalejo <michelmolina65@gmail.com>
2020-01-18 16:37:51 -0500

    Create 2-key_index.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+ * key_index - Function implementing the djb2 algorithm.
+ * @key: Key is the key.
+ * @size: Size is the size of the array of the hash table.
+ * Return: Index at which the key/value pair should be stored.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+
+}
