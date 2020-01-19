30244041daa734884756f196cd855cd7ef86f4e8
michelalejo <michelmolina65@gmail.com>
2020-01-19 18:32:40 -0500

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,8 +8,8 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int slot = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *new = ht->array[slot];
+	unsigned long int idx = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *new = ht->array[idx];
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
