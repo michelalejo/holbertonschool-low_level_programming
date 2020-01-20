97493f294293cfab02ae4aedf9e0f4e80544d911
michelalejo <michelmolina65@gmail.com>
2020-01-19 21:15:18 -0500

    Create 5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - Function that prints a hash table.
+ * @ht: Hash table you want to add or update.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp;
+	char *arr = "";;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			printf("%s'%s': '%s'", arr, tmp->key, tmp->value);
+			arr = ", ";
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
+}
